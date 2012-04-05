ec04c40e8f333d2d41a964943a85bd2e4cadf8e1
Anton Bravin <baboonweb@gmail.com>
2012-04-05 16:32:43 +0400

    first

---
+++ frontend/muddler.js
@@ -0,0 +1,43 @@
+function nowTime(cb) {
+	var date = new Date();
+	var hour = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	if(hour < 10) {
+		hour = '0' + hour;
+	}
+	if(min < 10) {
+		min = '0' + min;
+	}
+	if(sec < 10) {
+		sec = '0' + sec;
+	}
+	cb('['+ hour + ':'+ min + ':' + sec + ']');
+}
+
+function sendMessage() {
+	var board = document.getElementById('board');
+		var input = document.getElementById('inp');
+		nowTime(function(time) {
+			var html= '<div class="row"><div class="time">'+time+'</div><div class="chat">[loc]</div><div class="sender">[<span class="sender_name">me</span>]:</div>';
+			html+= '<span class="message">'+input.value+'</span>';
+			html+= '</div>';
+			board.innerHTML+= html;
+			board.scrollTop = 9999;
+			input.value = '';
+		});
+}
+
+$(document).ready(function() {
+
+	$('#inp').keyup(function(e) {
+    if(e.keyCode == 13){
+ 			sendMessage(); 
+    }
+  });
+
+	$('#but').click(function() {
+		sendMessage();
+	});
+
+});
